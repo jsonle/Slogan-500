efbd9b9da11faed0303f0de2a88ff4fa45e73bef
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-09-30 16:28:58 -0500

    indexes

--- slogan-500-api/app/controllers/users_controller.rb
+++ slogan-500-api/app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    def create
+         
+    end
+    def index
+        users = User.all 
+        render json: users, include: [:scores]
+    end 
 end
--- slogan-500-api/config/routes.rb
+++ slogan-500-api/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :slogans
   resources :companies
   resources :scores
   resources :users

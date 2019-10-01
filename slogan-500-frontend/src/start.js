4b868ef8ab04310ec82db41187c86a6560e3907c
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-10-01 14:51:47 -0500

    first phase of game complete

--- slogan-500-frontend/src/start.js
+++ slogan-500-frontend/src/start.js
@@ -1,6 +1,12 @@
 let companiesTest;
 let companiesURL = "http://localhost:3000/companies"
 let roundcomp = "http://localhost:3000/companies/show"
+//selecting buttons and slogan area
+
+
+
+
+
 let login = document.getElementById("login-input")
 let div = document.getElementById("left-container")
 // login is grabbing the submit button for login
@@ -8,7 +14,7 @@ let startbtn = document.getElementById("play-button")
 // startbtn is the game start button
 document.addEventListener("DOMContentLoaded", (event) => {
 
-submit();
+
 startup();
 
 
@@ -25,18 +31,46 @@ function startup() {
     ul.appendChild(li)
     startbtn.addEventListener("click", (event) => {
         event.preventDefault(); 
+        console.log("hello")
         fetch(roundcomp)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(options){
-       console.log(options)
-    })
+        .then(function(response){
+            return response.json()
+        })
+            .then(function(options){
+                //these are the four random companies and their slogans 
+                
+                            let sloganhead = document.createElement("h3")
+                            let slogan = document.createElement("h2")
+                            slogan.setAttribute("id", "slogan-head")
+                            sloganhead.innerText = "Slogan:"
+                            div.appendChild(sloganhead)
+                            div.appendChild(slogan)
+
+
+
+                let randoslogan = Math.floor ( Math.random() * 4 )
+                 slogan.innerText = options[randoslogan].slogan
+
+
+                    options.forEach(function(option) {
+                     let answerbutton = document.createElement("button")
+                        answerbutton.setAttribute("class", "btn btn-primary btn-lg")
+                        answerbutton.setAttribute("id", "answerbtn")
+                    answerbutton.innerText = option.name
+                    div.appendChild(answerbutton)
+                    
+                    
+                    
+                    })
+            
+                
+            })
     
     
     
-})  
+    })  
 }
+
 
 
 
